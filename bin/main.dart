950198fcb6d4720db1f9d446d99fbc02feeb59d7
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-16 20:18:05 +0300

    add example in main

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,75 @@
-main(){
+import 'model/bmw.dart';
+import 'model/car.dart';
+import 'dart:io';
 
+void main() {
+  List<Car> cars = [];
+
+// Display options to the user
+  print('1. Display Cars');
+  print('2. Create a Car');
+  print('3. Update a Car');
+  print('4. Search for a Car');
+  print('5. Exit');
+
+  int option = 0;
+  while (option != 5) {
+    print('\nEnter your option: ');
+    option = int.parse(stdin.readLineSync()!);
+    switch (option) {
+      case 1:
+        print('\nDisplaying Cars:');
+        for (var car in cars) {
+          car.display();
+        }
+        break;
+      case 2:
+        print('\nCreating a Car:');
+        print('Enter car name: ');
+        String? name = stdin.readLineSync();
+        print('Enter car description: ');
+        String? description = stdin.readLineSync();
+        // Create a new car based on user input
+        Car newCar = BMW(name!, description!); // Example: Create a BMW car
+        cars.add(newCar);
+        break;
+      case 3:
+        print('\nUpdating a Car:');
+        print('Enter car name to update: ');
+        String? carName = stdin.readLineSync();
+        // Find the car to update
+        Car? carToUpdate = cars.firstWhere((car) => car.name == carName, orElse: () => BMW('name', 'description'));
+        if (carToUpdate != null) {
+          print('Enter new car name: ');
+          String? newName = stdin.readLineSync();
+          print('Enter new car description: ');
+          String? newDescription = stdin.readLineSync();
+          carToUpdate.update(newName!, newDescription!);
+          print('Car updated successfully!');
+        } else {
+          print('Car not found!');
+        }
+        break;
+      case 4:
+        print('\nSearching for a Car:');
+        print('Enter search query: ');
+        String? query = stdin.readLineSync();
+        List<Car> searchResults = cars.where((car) => car.search(query!)).toList();
+        if (searchResults.isNotEmpty) {
+          print('Search Results:');
+          for (var car in searchResults) {
+            car.display();
+          }
+        } else {
+          print('No cars found matching the search query.');
+        }
+        break;
+      case 5:
+        print('\nExiting...');
+        break;
+      default:
+        print('Invalid option. Please try again.');
+        break;
+    }
+  }
 }
